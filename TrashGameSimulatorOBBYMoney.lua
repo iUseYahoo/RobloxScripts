e114240794dee6d21a1752d92e3f36d9bb5383b3
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-01-18 17:52:17 +1100

    Create TrashGameSimulatorOBBYMoney.lua

---
+++ TrashGameSimulatorOBBYMoney.lua
@@ -0,0 +1,14 @@
+local dia = game:GetService("Workspace").Diamond
+local plr = game.Players.LocalPlayer
+local head = plr.Character.Head
+local timetowait = 1
+
+getgenv().toggle = false -- make true to enable then execute
+-- make false to disable then re execute this script
+
+while getgenv().toggle == true do
+    task.wait(timetowait)
+    head.CFrame = CFrame.new(dia.Position)
+end
+
+print("retard")
